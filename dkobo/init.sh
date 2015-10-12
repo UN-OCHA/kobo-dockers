408d8e344b8779f5da4964b31a5fe7dee853df17
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-10-12 14:12:51 +0300

    base-dkobo: removed bower and put it in the final container in a script

--- dkobo/init.sh
+++ dkobo/init.sh
@@ -3,6 +3,8 @@
 oldpwd=$(pwd)
 cd /srv/src/koboform
 
+npm install -g --save-dev
+npm install -g bower --allow-root
 bower install --config.interactive=false --allow-root
 
 python manage.py syncdb --noinput
