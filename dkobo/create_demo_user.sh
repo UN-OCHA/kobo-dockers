dd568c5c1006b3cbb60897d0f3a8ebe6b081a5a2
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-10-13 16:13:10 +0300

    dkobo & kobocat: added sync_static.sh; dkobo: added create_demo_user.sh

---
+++ dkobo/create_demo_user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[ -z "$DEFAULT_KOBO_USER" ] && exit 1
+
+[ -z "$DEFAULT_KOBO_PASS" ] && exit 2
+
+echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER').count() > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" \
+    | python manage.py shell 2>&1
+
+
---
+++ kobocat/sync_static.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+oldpwd=$(pwd)
+cd /srv/src/kobocat
+
+echo "Collecting static files..."
+python manage.py collectstatic -v 0 -c --noinput 
+echo "Done"
+echo "Fixing permissions..."
+chown -R wsgi /srv/srv/koboform
+echo "Done."
+echo "Syncing to nginx folder..."
+rsync -aq /srv/src/kobocat/onadata/static/* /srv/static/
+echo "Done"
+
+cd $oldpwd
