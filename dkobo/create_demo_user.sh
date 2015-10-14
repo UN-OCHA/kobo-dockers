e92601c99f2d604bcebc38f68f73c67a3b17fb82
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-10-14 13:11:54 +0300

    added a cd to path on create_demo_user script.

--- dkobo/create_demo_user.sh
+++ dkobo/create_demo_user.sh
@@ -4,6 +4,8 @@
 
 [ -z "$DEFAULT_KOBO_PASS" ] && exit 2
 
+cd /srv/srv/koboform
+
 echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER').count() > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" \
     | python manage.py shell 2>&1
 
