f313b6ce180459f5990c170d280182f4f80b346e
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-10-14 13:52:15 +0300

    dkobo: fixed typo in create_demo_user.sh

--- dkobo/create_demo_user.sh
+++ dkobo/create_demo_user.sh
@@ -4,7 +4,7 @@
 
 [ -z "$DEFAULT_KOBO_PASS" ] && exit 2
 
-cd /srv/srv/koboform
+cd /srv/src/koboform
 
 echo "from django.contrib.auth.models import User; print 'UserExists' if User.objects.filter(username='$DEFAULT_KOBO_USER').count() > 0 else User.objects.create_superuser('$DEFAULT_KOBO_USER', 'kobo@example.com', '$DEFAULT_KOBO_PASS');" \
     | python manage.py shell 2>&1
