52757f7c4edae670d2a95b1eb182a161fed59438
John N. Milner <john@tmoj.net>
2016-11-21 14:16:54 -0500

    Explicitly set the mode of /tmp in the container

--- kobocat/init.sh
+++ kobocat/init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+chmod 777 /tmp # Per OPS-1378
+
 oldpwd=$(pwd)
 cd /srv/src/kobocat
 
 psql -h db -p ${PSQL_PORT} -U ${KOBO_PSQL_DB_USER} ${KOBO_PSQL_DB_NAME} -c "create extension postgis; create extension postgis_topology"
- 
+
 python manage.py syncdb --noinput
 
 python manage.py migrate --noinput
